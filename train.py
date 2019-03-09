c6171b422a423928b63224314cbfac2b7aac0665
dstevens08 <chodankar.shubham@gmail.com>
2019-03-09 19:43:15 +0530

    NN training code

--- train.py
+++ train.py
@@ -53,6 +53,8 @@ num_filters_conv4=32
 filter_size_conv5=2
 num_filters_conv5=32
 
+fc_layer_size=1024
+
 #Creating weights
 def create_weights(shape):
 	return tf.variable(tf.truncated_normal(shape,stddev=0.05))
@@ -94,7 +96,58 @@ def create_fc_layer(input,num_inputs,num_outputs,use_relu=True):
 	bias=create_biases(num_outputs)
 	#Fully connected layer takes input x and produces wx+b.WE use matmul to multiply matrices
 	layer=tf.matmul(input,weights)+bias
-	layer=tf.nn.relu(layer)
+	if use_relu:
+		layer=tf.nn.relu(layer)
 	return layer
+
+#Convolution Layer and Max Pooling Layer 1
+layer_conv1=create_convolution_layer(input=x,num_input_channels=num_channels,conv_filter_size=filter_size_conv1,num_filters=num_filters_conv1)
+
+#Convolution Layer and Max Pooling Layer 2
+layer_conv2=create_convolution_layer(input=layer_conv1,num_input_channels=num_filters_conv1,conv_filter_size=filter_size_conv2,num_filters=num_filters_conv2)
+
+#Convolution Layer and Max Pooling Layer 3
+layer_conv3=create_convolution_layer(input=layer_conv2,num_input_channels=num_filters_conv2,conv_filter_size=filter_size_conv3,num_filters=num_filters_conv3)
+
+#Convolution Layer and Max Pooling Layer 4
+layer_conv4=create_convolution_layer(input=layer_conv3,num_input_channels=num_filters_conv3,conv_filter_size=filter_size_conv4,num_filters=num_filters_conv4)
+
+#Convolution Layer and Max Pooling Layer 5
+layer_conv5=create_convolution_layer(input=layer_conv4,num_input_channels=num_filters_conv4,conv_filter_size=filter_size_conv5,num_filters=num_filters_conv5)
+
+#Flattening Layer
+layer_flat=create_flatten_layer(layer_conv5)
+
+#Fully connected layer 1
+layer_fc1=create_fc_layer(input=layer_flat,num_inputs=layer_flat.get_shape()[1:4].num_elements(),num_outputs=fc_layer_size,use_relu=True)
+
+#Fully connected layer 2
+layer_fc2=create_fc_layer(input=layer_fc1,num_inputs=fc_layer_size,num_outputs=num_classes,use_relu=False)
+
+#Output Layers
+y_pred=tf.nn.softmax(layer_fc2,name='y_pred')
+y_pred_cls=tf.argmax(y_pred,dimension=1) #Largest y_pred value classes,will be selected
+
+Session.run(tf.global_variables_initializer())
+
+#Error calculation and learning
+cross_entropy=tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2,labels=y_true)
+cost=tf.reduce_mean(cross_entropy)
+optimizer=tf.train.AdamOptimizer(learning_rate=0.01).minimize(cost)
+correct_prediction=tf.equal(y_pred_cls,y_true_cls)
+accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+Session.run(tf.global_variables_initializer())
+
+
+
+
+
+
+
+
+
+
+
+
 
 
