c7e861d8c1bab60f0623ba1e03fd4f5901026d2f
Abhibongale <bongale.abhi97@gmail.com>
2019-03-12 15:16:15 +0530

    Dataset code updated

--- dataset.py
+++ dataset.py
@@ -1,119 +1,35 @@
+import numpy as np
 import cv2
 import os
-import glob
-from sklearn.utils import shuffle
-import numpy as np
+import matplotlib.pyplot as plt
+import random
 
+DATADIR = "./Traindata"
+CATEGORIES = ['Gesture_0','Gesture_1','Gesture_2','Gesture_3','Gesture_4','Gesture_5','Gesture_6','Gesture_7','Gesture_8','Gesture_9']
+#classes=['Gesture-0','Gesture-1','Gesture-2','Gesture-3','Gesture-4','Gesture-5','Gesture-6','Gesture-7','Gesture-8','Gesture-9']
+IMG_SIZE=50
+training_data=[]
+def creating_training_data():
+  for category in CATEGORIES:
+    path = os.path.join(DATADIR,category)
+    num_classes = CATEGORIES.index(category)
+    print(num_classes)
+    for img in os.listdir(path):
+      img_array = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
+      new_array = cv2.resize(img_array,(IMG_SIZE,IMG_SIZE))
+      training_data.append([new_array,num_classes])
+            
+creating_training_data()
+#print(len(training_data))
+random.shuffle(training_data)
+"""for sample  in training_data[:10]:
+  print(sample[1])
+"""
 
-def load_train(train_path, image_size, classes):
-    images = []
-    labels = []
-    img_names = []
-    cls = []
-
-    print('Going to read training images')
-    for fields in classes: 
-	      #print(fields)  
-        index = classes.index(fields)
-        print("index")
-	      #print(index)
-        print('Now going to read {} files (Index: {})'.format(fields, index))
-        path = os.path.join(train_path, fields, '*g')
-        files = glob.glob(path)
-        for fl in files:
-            image = cv2.imread(fl)
-            image = cv2.resize(image, (image_size, image_size),0,0, cv2.INTER_LINEAR)
-            image = image.astype(np.float32)
-            image = np.multiply(image, 1.0 / 255.0)
-            images.append(image)
-            label = np.zeros(len(classes))
-            label[index] = 1.0
-            labels.append(label)
-            flbase = os.path.basename(fl)
-            img_names.append(flbase)
-            cls.append(fields)
-    images = np.array(images)
-    labels = np.array(labels)
-    img_names = np.array(img_names)
-    cls = np.array(cls)
-    print(images,'/n',labels,'/n',img_names,'/n',cls)
-    return images, labels, img_names, cls
-
-
-class DataSet(object):
-
-  def __init__(self, images, labels, img_names, cls):
-    self._num_examples = images.shape[0]
-
-    self._images = images
-    self._labels = labels
-    self._img_names = img_names
-    self._cls = cls
-    self._epochs_done = 0
-    self._index_in_epoch = 0
-
-  @property
-  def images(self):
-    return self._images
-
-  @property
-  def labels(self):
-    return self._labels
-
-  @property
-  def img_names(self):
-    return self._img_names
-
-  @property
-  def cls(self):
-    return self._cls
-
-  @property
-  def num_examples(self):
-    return self._num_examples
-
-  @property
-  def epochs_done(self):
-    return self._epochs_done
-
-  def next_batch(self, batch_size):
-    """Return the next `batch_size` examples from this data set."""
-    start = self._index_in_epoch
-    self._index_in_epoch += batch_size
-
-    if self._index_in_epoch > self._num_examples:
-      # After each epoch we update this
-      self._epochs_done += 1
-      start = 0
-      self._index_in_epoch = batch_size
-      assert batch_size <= self._num_examples
-    end = self._index_in_epoch
-
-    return self._images[start:end], self._labels[start:end], self._img_names[start:end], self._cls[start:end]
-
-
-def read_train_sets(train_path, image_size, classes, validation_size):
-  class DataSets(object):
-    pass
-  data_sets = DataSets()
-
-  images, labels, img_names, cls = load_train(train_path, image_size, classes)
-  images, labels, img_names, cls = shuffle(images, labels, img_names, cls)  
-
-  if isinstance(validation_size, float):
-    validation_size = int(validation_size * images.shape[0])
-
-  validation_images = images[:validation_size]
-  validation_labels = labels[:validation_size]
-  validation_img_names = img_names[:validation_size]
-  validation_cls = cls[:validation_size]
-
-  train_images = images[validation_size:]
-  train_labels = labels[validation_size:]
-  train_img_names = img_names[validation_size:]
-  train_cls = cls[validation_size:]
-
-  data_sets.train = DataSet(train_images, train_labels, train_img_names, train_cls)
-  data_sets.valid = DataSet(validation_images, validation_labels, validation_img_names, validation_cls)
-
-  return data_sets
+X = []
+y = []
+for features,labels in training_data:
+  X.append(features)
+  y.append(labels)
+X = np.array(X).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+print(X.shape)
--- train.py
+++ train.py
@@ -171,7 +171,7 @@ def train(num_iteration):
             epoch = int(i / int(data.train.num_examples/batch_size))    
             
             show_progress(epoch, feed_dict_tr, feed_dict_val, val_loss)
-            saver.save(session,'./Hands-Gesture-Recognition-Using-Neural-Network/Train_model') 
+            saver.save(Session,'./Hands-Gesture-Recognition-Using-Neural-Network/Train_model') 
 
 
     total_iterations += num_iteration
